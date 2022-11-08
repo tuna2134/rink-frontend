5e1a69a1e85adfa9413e324e8aef8e0c63e9fdb6
mc_fdc <Masato.04.11.2007@gmail.com>
2022-11-08 21:17:54 +0900

    Update create.tsx

--- pages/create.tsx
+++ pages/create.tsx
@@ -1,9 +1,11 @@
 import axios from 'axios';
 import { API_URL } from 'lib';
+import { setCookie } from 'nookies'
+import RootPage from '../components/root'
 
 
 export default function accountCreate() {
-  async function hundleSubmit(event) {
+  async function handleSubmit(event) {
     const r = await axios.post(`${API_URL}/v1/accounts`, {
       data: {
         email: event.target.email.value,
@@ -11,5 +13,23 @@ export default function accountCreate() {
         password: event.target.password.value,
       }
     });
+    setCookie(null, "token", r.data);
   }
+  return (
+    <RootPage>
+      <h2>アカウント作成</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="userid">ユーザーID</label>
+        <input type="text" id="userid" name="id" required />
+        
+        <label htmlFor="email">メールアドレス</label>
+        <input type="text" id="email" name="email" required />
+        
+        <label htmlFor="password">パスワード</label>
+        <input type="password" id="password" name="password" required />
+        
+        <button type="submit">作成</button>
+      </form>
+    </RootPage>
+  );
 }
